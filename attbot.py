e9676b33a0f44de7d5cadd677a3e8e1614c06272
roesta07 <ro.esta07@gmail.com>
2021-09-29 09:41:02 +0545

    changes

--- attbot.py
+++ attbot.py
@@ -1,7 +1,8 @@
-import pandas as pd
-import request
+import json
+import requests
 
-webhook_url='https://hooks.zapier.com/hooks/catch/6891865/b6juuuh/'
+webhook_url='https://hooks.zapier.com/hooks/catch/6871459/b6vht3q'
 data={'name':['Binay','sujan']}
 r=requests.post(webhook_url,data=json.dumps(data),
                 headers={'Content-Type':'application/json'})
+print(r.status_code)
